c107e56540fe9e65fbd305a095679dcfb6337252
ix1osbek <bekerkinov2004@gmail.com>
2025-07-09 03:08:44 +0500

    test

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,10 @@ async function bootstrap() {
 
   const PORT = process.env.PORT ?? 3000
   await app.listen(PORT , ()=>{
-    console.log(`Server is running on port ${PORT}`)
+    console.log(`Server is running on port yondi ${PORT}`)
     
   });
 }
-bootstrap();
+bootstrap()
+
+
