82eaaef12ede7c5254f1004e92a22b0f1b6f716e
ix1osbek <bekerkinov2004@gmail.com>
2025-07-13 15:45:44 +0500

    Auth role section added

--- src/auth/entities/auth.entity.ts
+++ src/auth/entities/auth.entity.ts
@@ -1,21 +1,26 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Role } from '../role.enum';
 @Entity()
 export class Auth {
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
 
-  @Column({ unique: true })
-  email: string;
+    @Column({ unique: true })
+    email: string;
 
-  @Column()
-  password: string;
+    @Column()
+    password: string;
 
-  @Column({ default: false })
-  isVerified: boolean;
+    @Column({ default: false })
+    isVerified: boolean;
 
-  @Column({ nullable: true })
-  otp: string;
+    @Column({ nullable: true })
+    otp: string;
 
-  @Column({ nullable: true, type: 'timestamp' })
-  otpTime: Date;
+    @Column({ nullable: true, type: 'timestamp' })
+    otpTime: Date;
+
+    @Column({ type: 'enum', enum: Role, default: Role.USER })
+    role: Role;
+
 }
