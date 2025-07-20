cdd4d7bd4cfd7ab78a4636766391b6e754601e6f
ix1osbek <bekerkinov2004@gmail.com>
2025-07-20 23:56:36 +0500

    There has been a change in the "About" section.

--- src/about/about.controller.ts
+++ src/about/about.controller.ts
@@ -1,7 +1,11 @@
-import { Controller, Post, Get, Body } from '@nestjs/common';
+import { Controller, Post, Get, Body, UseGuards } from '@nestjs/common';
 import { AboutService } from './about.service';
 import { CreateAboutDto } from './dto/create-about.dto';
-import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { Role } from 'src/auth/role.enum';
 
 @ApiTags('About')
 @Controller('api/about')
@@ -9,7 +13,14 @@ export class AboutController {
     constructor(private readonly aboutService: AboutService) { }
 
     @Post('create')
+    @UseGuards(AuthGuard("jwt"), RolesGuard)
+    @Roles(Role.SUPERADMIN, Role.ADMIN)
     @ApiOperation({ summary: 'Sayt haqida ma’lumot yaratish' })
+    @ApiOperation({ summary: 'Faqat superadmin va admin huquqi bor!' })
+    @ApiResponse({ status: 201, description: 'About muvofaqiyatli qoshildi' })
+    @ApiResponse({ status: 500, description: 'Serverda xato yuz berdi' })
+    @ApiResponse({ status: 401, description: 'Ruxsat etilmagan' })
+    @ApiBearerAuth()
     create(@Body() dto: CreateAboutDto) {
         return this.aboutService.create(dto);
     }
